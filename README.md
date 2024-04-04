69a5d57d4e78ee64187525d19bba1eb0943c953b
Zaim Beshich <zaimbeshich93@gmail.com>
2024-04-04 12:19:19 +0400

    fix config and add react navigation

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 ## Step 1: Start the Metro Server
 
 First, you will need to start **Metro**, the JavaScript _bundler_ that ships _with_ React Native.
+You will need need Node.js >= 18
 
 To start Metro, run the following command from the _root_ of your React Native project:
 
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,33 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    // [
+    //   '@babel/plugin-proposal-decorators',
+    //   {
+    //     legacy: true,
+    //   },
+    // ],
+    [
+      'babel-plugin-inline-import',
+      {
+        extensions: ['.svg'],
+      },
+    ],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@types': './src/types',
+          '@navigation': './src/navigation',
+          '@images': './src/images',
+          '@icons': './src/icons',
+        },
+      },
+    ],
+    // 'react-native-reanimated/plugin',
+  ],
 };
